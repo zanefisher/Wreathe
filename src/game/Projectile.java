c7ac1d17786e22febd0bd603047ae71a4cdd274e
BoomYan <jiamingyan@hotmail.com>
2015-04-23 23:26:17 -0400

    address bugs at projectile after target dies

--- src/game/Projectile.java
+++ src/game/Projectile.java
@@ -7,6 +7,8 @@ public class Projectile extends GameObject{
 	float distance = 0f;
 	float attackPower = 5f;
 	static int defaultColor = sketch.color(0, 99, 99);
+	float fixedDx = 0f;
+	float fixedDy = 0f;
 	
 	Projectile(Sketch s, GameObject a, Obstacle b) {
 		sketch = s;
@@ -23,16 +25,44 @@ public class Projectile extends GameObject{
 	public boolean update() {
 		
 		distance = Sketch.dist(x, y, to.x, to.y);
-		dx = attackSpeed*(to.x-x)/distance;
-		dy = attackSpeed*(to.y-y)/distance;
+		if(fixedDx == 0){
+			dx = attackSpeed*(to.x-x)/distance;
+			dy = attackSpeed*(to.y-y)/distance;
+		}
+		else{
+			dx = fixedDx;
+			dy = fixedDy;
+		}
 		x += dx;
 		y += dy;
 		distance = Sketch.dist(x, y, to.x, to.y);
-		if(distance>to.radius)	return true;
-		else {
+
+		if(to.obstacleLife<=0 && fixedDx ==0){
+			fixedDx = dx;
+			fixedDy = dy;
+		}
+		
+		if(fixedDx == 0 && distance<=to.radius+this.radius){
+			//hit the current Obstacle
 			Obstacle tmp = to;
 			tmp.obstacleLife -= attackPower;
 			return false;
 		}
+		
+		if(fixedDx != 0 ){
+			for (int i = 0; i < sketch.world.contents.size(); ++i) {
+				GameObject other = sketch.world.contents.get(i);
+				if (other instanceof Obstacle && distTo(other)<=0) {
+					Obstacle tmp = (Obstacle)other;
+					tmp.obstacleLife -= attackPower;
+					return false;
+				}
+			}
+		}
+		//if outside of the world
+		if(Sketch.dist(0, 0, x, y)>sketch.world.radius) return false;
+			
+
+		return true;
 	}
 }
